9d0d275cb14acf604f9e1ffe153b91a1ccb629e9
Ana Ramirez <aramirez.cac@gmail.com>
2023-05-18 14:57:00 -0400

    clase 5, reasignacion y redeclaracion

---
+++ src/scope/reasignacion.js
@@ -0,0 +1,38 @@
+var firstName; //Undefined
+firstName = 'Ana';
+console.log(firstName);
+
+var lastName = "Beatriz"; //declarar / asignar
+lastName = 'Luis'; // reasignar
+console.log(lastName);
+
+
+var secondName ='Isaac'; //declarando / asignando 
+var secondName ='Daniel'; //reasignando
+console.log(secondName);
+
+//////////////////////////////////////////////
+//let
+let fruit = 'Apple'; //declarar y asignar
+fruit ='Kiwi'; //reasignar
+console.log(fruit);
+
+//let fruit = 'Banana' //no se puede redecladar una variable con el let
+
+
+/////////////////////////////////////////////////
+//const
+
+const animal ='dog'; //declarar y asignar
+animal ='cat';  /// en const no se puede asignar
+//const animal ='Snake'; //no se puede redeclarar en const
+console.log(animal);
+
+
+const vehicles = [];
+vehicles.push("Hola","❤"); //agregar
+console.log(vehicles);
+
+vehicles.pop(); //saca al ultimo elemento del arrays
+console.log(vehicles);
+
