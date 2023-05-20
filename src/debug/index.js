7f5d598a40a08cb8f69bdee48e813872704336b0
Ana Ramirez <aramirez.cac@gmail.com>
2023-05-20 08:20:19 -0400

    debugging

---
+++ src/debug/index.js
@@ -0,0 +1,16 @@
+
+
+var a = 'Hello';
+
+function hello(){
+    let b ='Hello world';
+    const c = 'Helloo';
+    debugger;
+}
+
+hello();
+
+
+// en el navegador en la brra de busqueda ponemos 
+// about:blank
+// para tener una paguna en blanco, esto nos ayuda a podes debbugear
